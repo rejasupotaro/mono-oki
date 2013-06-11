88b027655404d27919826f3deb141eebb2fcaed7
Kentaro Takiguchi <takiguchi0817@gmail.com>
2013-06-11 22:19:28 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MonoOki::Application.config.secret_token = 'e6700b0ad2434f164e65e648c2c28fc4fad655f2d3729392e6616f519f5c0b710d9cda724739a7b7997a184c010ad5cdd6721c590bf9b2c4c54ceff6b2f40097'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MonoOki::Application.config.session_store :cookie_store, key: '_mono-oki_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MonoOki::Application.config.session_store :active_record_store
